8573fddecba7c5ba5e3bdd19ff0633752d076574
Monegasik <120821453+Monegasik@users.noreply.github.com>
2023-06-08 18:11:45 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -1,126 +1,84 @@
-import pygame
-import time
-from random import randint
- 
-pygame.init()
- 
+from pygame import *
 
-back = (200, 255, 255) #цвет фона (background)
-mw = pygame.display.set_mode((700, 500)) #окно программы (main window)
-mw.fill(back)
-clock = pygame.time.Clock()
+black = (0, 0, 0)
+white = (24, 217, 175)
+clock = time.Clock()
+FPS=60
+font.init()
+font1 = font.SysFont('Arial', 80)
+win1 = font1.render('LEFT WIN!', True, black)
+win2 = font1.render('RIGHT WIN!', True, black)
 
+win_width = 1000
+win_height = 700
+screen = display.set_mode((win_width, win_height))
+display.set_caption("Ping Pong")
 
+#background = transform.scale(image.load(white), (win_width, win_height))
 
-#окончание игры
-game_over = False
-moving_up = False
-moving_down = False
+#Определение начальных координат мяча и ракеток
+ball_x = win_width // 2
+ball_y = win_height // 2
+ball_speed_x = 10
+ball_speed_y = 10
 
-ball_x = 3
-ball_y = 3
+rocket1_x = 50
+rocket1_y = win_height // 2 - 55
+rocket2_x = win_width - 80
+rocket2_y = win_height // 2 - 55
+rocket_speed = 5
 
-#координаты старта платформы
-platform_x1 = 350
-platform_y1 = 200
+#Цикл игры
+game = True
+win = False
+while game:
+    screen.fill(white)
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
 
-class Area():
-    def __init__(self, x = 0, y = 0, width = 10, height = 10, color = None):
-        self.rect = pygame.Rect(x, y, width, height) #прямоугольник
-        self.fill_color = back
-        if color:
-            self.fill.color = color
+    if not win:
+            # screen.fill(white)
 
-    def color(self, new_color):
-        self.fill_color = new_color
- 
-    def fill(self):
-        pygame.draw.rect(mw, self.fill_color, self.rect)
- 
-    # def collidepoint(self, x, y):
-    #     return self.rect.collidepoint(x, y)      
-
-    def colliderect(self, rect):
-        return self.rect.colliderect(rect)
-
-
-class Picture(Area):
-    def __init__(self, filename, x = 0, y = 0, width = 10, height = 10):
-        Area.__init__(self, x=x, y=y, width=width, height=height, color=None)
-        self.image = pygame.image.load(filename)
-    def update(self):
+        # Обработка движения ракеток
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_w] and rocket1_y > 0:
+            rocket1_y -= rocket_speed
+        if keys[K_s] and rocket1_y < win_height - 100:
+            rocket1_y += rocket_speed
+        if keys[K_UP] and rocket2_y > 0:
+            rocket2_y -= rocket_speed
+        if keys[K_DOWN] and rocket2_y < win_height - 100:
+            rocket2_y += rocket_speed
+
+        # Обработка движения мяча
+        ball_x += ball_speed_x
+        ball_y += ball_speed_y
+        if ball_x < 0 or ball_x > win_width - 20:
+            ball_speed_x = -ball_speed_x
+        if ball_y < 0 or ball_y > win_height - 20:
+            ball_speed_y = -ball_speed_y
+
+        # Обработка столкновения мяча с ракетками
+        if ball_x < rocket1_x + 20 and rocket1_y < ball_y < rocket1_y + 100:
+            ball_speed_x = -ball_speed_x
+
+        if ball_x > rocket2_x - 20 and rocket2_y < ball_y < rocket2_y + 100:
+            ball_speed_x = -ball_speed_x
+
+        if ball_x < 50:
+            screen.blit(win1,(400, 200))
+            win = True
 
 
-    def draw(self):
-        mw.blit(self.image, (self.rect.x, self.rect.y))
-    
 
+        if ball_x > 950:
+            screen.blit(win2,(400, 200))
+            win = True
 
-platform_left = Picture('platform3.png', 100, 100, 50, 50)
-platform_right = Picture('platform3.png', 400, 400, 50, 50)
-
-ball = Picture('ball.png', 250, 190, 50, 50)
-
-
-
-while not game_over:
-    ball.fill()
-    platform_left.fill()
-    platform_right.fill()
-
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                moving_up = True
-            if event.key == pygame.K_DOWN:
-                moving_down = True
-        
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                moving_up = False
-            if event.key == pygame.K_DOWN:
-                moving_down = False
-
-
-#движение платформы
-    if moving_down:
-        platform.rect.y -= 3
-
-    if moving_up:
-        platform.rect.y += 3
-
-
-# permanent ball moving
-    ball.rect.x += ball_x
-    ball.rect.y += ball_y
-
-# check borders, change direction if needs
-    if ball.rect.y < 0:
-        ball_y *= - 1
-
-    if ball.rect.x > 450 or ball.rect.x < 0:
-        ball_x *= - 1
-
-# check if ball touch the platform and change direction:
-    if (ball.rect.colliderect(platform_left.rect) or 
-    ball.rect.colliderect(platform_right.rect)):
-        ball_y *= - 1
-
-
-# draw platform and ball
-    platform_left.draw()
-    platform_right.draw()
-    ball.draw()
-
-    # renew scene
-    pygame.display.update()
-    clock.tick(40)
+# Отрисовка объектов на экране
+        draw.rect(screen, black, (rocket1_x, rocket1_y, 30, 120))
+        draw.rect(screen, black, (rocket2_x, rocket2_y, 30, 120))
+        draw.ellipse(screen, black, (ball_x, ball_y, 28, 28))
+        display.update()
+    clock.tick(FPS)
